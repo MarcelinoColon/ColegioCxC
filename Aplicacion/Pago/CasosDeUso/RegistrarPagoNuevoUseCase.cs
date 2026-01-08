f7db989f174bb5eb47709b064fe4e770deb2e362
Marcelino <Marcelino@LTP-CCTV-3>
2026-01-08 16:42:33 -0400

    The RegistraPagoNuevoUseCase class created is missing the implementation

--- Aplicacion/Pago/CasosDeUso/RegistrarPagoNuevoUseCase.cs
+++ Aplicacion/Pago/CasosDeUso/RegistrarPagoNuevoUseCase.cs
@@ -1,11 +1,35 @@
-﻿using Aplicacion.Interfaces.UseCase;
+﻿using Aplicacion.Interfaces.Repository;
+using Aplicacion.Interfaces.UseCase;
+using Aplicacion.Pago.DTOs;
+using Dominio;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace Aplicacion.Pago.CasosDeUso
 {
-    public class RegistrarPagoNuevoUseCase
+    public class RegistrarPagoNuevoUseCase : ICreateUseCase<PagoDto, PagoEntity>
     {
+        private readonly IReadRepository<CargoEntity> _readCargoRepository;
+        private readonly ICreateRepository<PagoEntity> _createPagoRepository;
+        private readonly ICreateRepository<TransaccionPagoEntity> _createTransaccionPagoRepository;
+        private readonly IUnitOfWork _UnitOfWork;
+
+        public RegistrarPagoNuevoUseCase(
+            IReadRepository<CargoEntity> readCargoRepository,
+            ICreateRepository<PagoEntity> createPagoRepository,
+            ICreateRepository<TransaccionPagoEntity> createTransaccionPagoRepository,
+            IUnitOfWork unitOfWork)
+        {
+            _readCargoRepository = readCargoRepository;
+            _createPagoRepository = createPagoRepository;
+            _createTransaccionPagoRepository = createTransaccionPagoRepository;
+            _UnitOfWork = unitOfWork;
+        }
+
+        public Task AddAsync(PagoDto dto)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
---
+++ Aplicacion/Pago/DTOs/PagoDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aplicacion.Pago.DTOs
+{
+    public class PagoDto
+    {
+    }
+}
